340b195aa5adff4b2c702367b9dcdc04bbc0ebea
david-allison12 <dgreenal@gmail.com>
2021-08-03 21:52:11 -0400

    Created HTML and CSS layout add steps on what needs to be completed in JS file

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//Things that need to happen
+
+// Input name to start the game
+// Luffy needs to bounce around the screen
+// As he bounces, his hunger, sleep, and boredom increase
+    //These all should increase at different levels (knowing Luffy, hunger will increase quickly)
+    //WHEN ANY OF THEM REACH 10 LUFFY DIES/IS DEFEATED (LETS FIGURE OUT SOME COOL WAY TO DO THIS)
+//After a certain period of time the age will increment as well
+    //When he ages the sprite will change to teen luffy -> gear 4 luffy or balloon luffy
+
+//When buttons are clicked, they should decrease their respective section
+
+const tamagotchi = {
+
+}
+
+const buttonBegin
